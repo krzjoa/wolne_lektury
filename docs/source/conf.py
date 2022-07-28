48620720f0e814b2ad8bae65d8b4016d033e4656
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2022-07-28 23:38:29 +0200

    Docs fixed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    #'myst_parser',
+    'myst_parser',
     'sphinx.ext.autosummary',
     'numpydoc'
 ]
